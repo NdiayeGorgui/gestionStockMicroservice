7f9aa1defce0f91af48aa9043a8359923e6d67a6
Gorgui Ndongo <gorguindongondiaye@gmail.com>
2021-09-27 01:41:35 -0400

    création de IllRepository dans billing service

---
+++ billing-service/src/main/java/com/gorgui/billingservice/repository/BillRepository.java
@@ -0,0 +1,9 @@
+package com.gorgui.billingservice.repository;
+
+import com.gorgui.billingservice.model.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface BillRepository extends JpaRepository<Bill,Long> {
+}
