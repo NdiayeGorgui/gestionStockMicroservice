e940b2c7478758a1840eb81dc1bbbb365aeaf2c7
Gorgui Ndongo <gorguindongondiaye@gmail.com>
2021-09-27 01:44:34 -0400

    création de l'interface customerservice dans billing service

---
+++ billing-service/src/main/java/com/gorgui/billingservice/service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.gorgui.billingservice.service;
+
+import com.gorgui.billingservice.model.Customer;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "CUSTOMER-SERVICE")
+public interface CustomerService {
+    @GetMapping("/customers/{id}")
+    public Customer findCustomerById(@PathVariable(name = "id") Long id);
+}
